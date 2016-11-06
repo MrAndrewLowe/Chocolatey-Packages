436c13a4e67dee45575a5e9d419710d8b335e0a8
Lowe <alowe2@kpmg.com>
2016-11-06 17:14:18 +1100

    New Version

---
+++ ChromeDriver2/2.25/ChromeDriver2.2.25/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'ChromeDriver2'
+$url = 'https://chromedriver.storage.googleapis.com/2.25/chromedriver_win32.zip' 
+$validExitCodes = @(0)
+$checksum = '2727729883ac960c2edd63558f08f601'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -checksum "$checksum"
